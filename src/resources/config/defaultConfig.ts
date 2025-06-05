04efdf7a8a00ed0431f05a514c701cf283d5a39c
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-05 00:09:26 -0600

    Add default configuration file with user preferences for language, themes, font, and text settings

---
+++ src/resources/config/defaultConfig.ts
@@ -0,0 +1,12 @@
+export const defaultConfig = {
+  preferredLanguage: "en",
+  preferredDarkTheme: "rose-pine",
+  preferredLightTheme: "rose-pine",
+  preferredFont: "JetBrains Mono",
+  preferredFontSize: 14,
+  preferredLineHeight: 1.5,
+  preferredLetterSpacing: 0.01,
+  preferredWordSpacing: 0.01,
+  preferredTextAlign: "left",
+  preferredTextTransform: "none",
+};
