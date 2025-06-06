028acdbfe65f03d31abccbbb9bb89d3d9c362a0c
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-06 01:33:15 -0600

    Enhance IntroWorkflow component with animated welcome messages and navigation icon for improved user experience

--- src/renderer/components/intro-workflow.tsx
+++ src/renderer/components/intro-workflow.tsx
@@ -1,3 +1,6 @@
+import { BlurFade } from "components/magicui/blur-fade";
+import { TypingAnimation } from "components/magicui/typing-animation";
+import { ArrowRightIcon } from "lucide-react";
 import { useState } from "react";
 
 export const IntroWorkflow = () => {
@@ -6,7 +9,7 @@ export const IntroWorkflow = () => {
   >("welcome");
 
   return (
-    <div>
+    <div className="flex h-screen w-screen items-center justify-center">
       <div>
         {workflowStep === "welcome" && (
           <Welcome onNext={() => setWorkflowStep("settings")} />
@@ -27,7 +30,32 @@ const Welcome = ({
 }: {
   onNext: React.Dispatch<React.SetStateAction<"welcome" | "settings" | "done">>;
 }) => {
-  return <div>Welcome</div>;
+  return (
+    <div>
+      <TypingAnimation
+        duration={35}
+        className="text-2xl font-bold"
+        startOnView
+        as="h1"
+      >
+        Welcome to Mongolore!
+      </TypingAnimation>
+      <div className="flex flex-row gap-2 items-center justify-center mt-2">
+        <TypingAnimation
+          delay={21 * 35}
+          duration={35}
+          className="text-sm font-bold text-muted-foreground"
+          startOnView
+          as="h2"
+        >
+          Let's get you started!
+        </TypingAnimation>
+        <BlurFade delay={((21 + 22) * 35 + 300) / 1000}>
+          <ArrowRightIcon className="h-4 w-4 animate-pulse" />
+        </BlurFade>
+      </div>
+    </div>
+  );
 };
 
 const Settings = ({
