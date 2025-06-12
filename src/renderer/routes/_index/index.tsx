daa078105decce4c4990fbb866f67590d80c62f9
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-12 00:08:40 -0600

    Update Index component layout to improve user experience by adding a message for unselected collections and adjusting the display structure for better visibility.

--- src/renderer/routes/_index/index.tsx
+++ src/renderer/routes/_index/index.tsx
@@ -8,10 +8,19 @@ export const Route = createFileRoute('/_index/')({
 function Index() {
   const { connectionId, connection } = useConnections()
   return (
-    <div className="p-2">
-      <pre className="text-xs">
+    <div className="relative h-full w-full">
+      <pre className="text-xs absolute top-0 left-0 w-full h-full">
         {JSON.stringify({ connectionId, connection }, null, 2)}
       </pre>
+
+      <div className="flex flex-col h-full w-full justify-center items-center">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-bold">No collections selected</h1>
+          <p className="text-sm text-muted-foreground">
+            Please select a collection to continue.
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
