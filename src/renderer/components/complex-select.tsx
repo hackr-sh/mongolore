806a9eb04bf76f5de51fa3eb60755c6c676a43d7
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-12 02:45:35 -0600

    Enhance ComplexSelect component by adding triggerClassName prop for customizable styling of the select trigger, improving flexibility in UI design.

--- src/renderer/components/complex-select.tsx
+++ src/renderer/components/complex-select.tsx
@@ -1,3 +1,4 @@
+import type { ClassValue } from 'clsx'
 import { useId } from 'react'
 
 import { Label } from 'renderer/components/ui/label'
@@ -8,23 +9,26 @@ import {
   SelectTrigger,
   SelectValue,
 } from 'renderer/components/ui/select'
+import { cn } from 'renderer/lib/utils'
 
 export default function ComplexSelect({
   label,
   placeholder,
   options,
+  triggerClassName,
   ...props
 }: {
   label?: string
   placeholder: string
   options: { label: string; value: string }[]
+  triggerClassName?: ClassValue
 } & React.ComponentProps<typeof Select>) {
   const id = useId()
   return (
     <div className="*:not-first:mt-2">
       {label && <Label htmlFor={id}>{label}</Label>}
       <Select {...props}>
-        <SelectTrigger id={id}>
+        <SelectTrigger id={id} className={cn(triggerClassName)}>
           <SelectValue placeholder={placeholder} />
         </SelectTrigger>
         <SelectContent className="[&_*[role=option]]:ps-2 [&_*[role=option]]:pe-8 [&_*[role=option]>span]:start-auto [&_*[role=option]>span]:end-2">
