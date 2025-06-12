e66326cc21a073b812b9656b3b445ab9cab556b0
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-06-12 00:02:44 -0600

    Refactor MasterDetail component to display selected connection name and improve layout. Integrate connection context and update UI structure for better user experience.

--- src/renderer/components/master-detail.tsx
+++ src/renderer/components/master-detail.tsx
@@ -1,20 +1,29 @@
-import { Outlet } from "@tanstack/react-router";
+import { Outlet } from '@tanstack/react-router'
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
-} from "renderer/components/ui/resizable";
+} from 'renderer/components/ui/resizable'
+import { useConnections } from 'renderer/providers/connections-provider'
 
 export const MasterDetail = () => {
+  const { connection } = useConnections()
   return (
-    <ResizablePanelGroup direction="horizontal">
-      <ResizablePanel className="p-4 mt-4" defaultSize={20}>
-        Master
-      </ResizablePanel>
-      <ResizableHandle />
-      <ResizablePanel className="p-4 mt-4">
-        <Outlet />
-      </ResizablePanel>
-    </ResizablePanelGroup>
-  );
-};
+    <div className="flex flex-col h-full w-full">
+      <div className="w-full pt-6 p-4 border-border border-b">
+        <h1 className="text-2xl font-bold">
+          {connection?.name ?? 'No connection selected'}
+        </h1>
+      </div>
+      <ResizablePanelGroup direction="horizontal">
+        <ResizablePanel className="p-4 mt-4" defaultSize={20}>
+          Master
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel className="p-4 mt-4">
+          <Outlet />
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </div>
+  )
+}
