6e2778efb6a657f26bec71d3b45c08a36a3a21da
Mohammad Al-Ahdal <mo@mohammad.dev>
2025-07-09 23:26:34 -0600

    Update RouteComponent to display selected database and collection information, enhancing user interface and context awareness.

--- src/renderer/routes/_index/$.tsx
+++ src/renderer/routes/_index/$.tsx
@@ -1,9 +1,25 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useDatabase } from 'renderer/providers/database-provider'
 
 export const Route = createFileRoute('/_index/$')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_index/$"!</div>
+  const { selectedDatabase, selectedCollection } = useDatabase()
+  return (
+    <div className="flex flex-col h-full w-full">
+      <h2 className="text-sm -mt-4 mb-4">
+        <span className="text-muted-foreground/70">
+          {selectedDatabase?.name} /{' '}
+        </span>
+        <span className="font-bold">{selectedCollection?.name}</span>
+      </h2>
+      <div className="flex flex-col gap-4 flex-1 justify-between overflow-y-auto">
+        <pre className="text-xl sticky top-0">documents: [</pre>
+        <div className="flex flex-col gap-4 flex-1">documents here</div>
+        <pre className="text-xl sticky bottom-0">];</pre>
+      </div>
+    </div>
+  )
 }
